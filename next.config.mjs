6a86e9e84484b1f8841eba8256866f032115c133
vibestudent <vibe.student.ai@gmail.com>
2025-10-21 22:21:50 +0300

    fixed 404 error locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
-  basePath: '/vibe-portfolio',
-  assetPrefix: '/vibe-portfolio/',
+  output: isProd ? 'export' : undefined,
+  basePath: isProd ? '/vibe-portfolio' : undefined,
+  assetPrefix: isProd ? '/vibe-portfolio/' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
